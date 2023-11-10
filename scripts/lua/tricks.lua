3a2c69dfe241499fadf14eff3052e48fea4ad485
rust-lang.xfoss.com <unisko@gmail.com>
2023-11-10 13:52:08 +0800

    Updated 'scripts/lua/pattern_matching.md'.

--- scripts/lua/tricks.lua
+++ scripts/lua/tricks.lua
@@ -6,3 +6,15 @@ print((string.gsub(test, "/%*.-%*/", "<COMMENT>")))
 
 i, j = string.find(";$%  **#$hello13", "%a*")
 print(i, j)
+
+pattern = string.rep("[^\n]", 70) .. "+"
+
+
+function nocase (s)
+    s = string.gsub(s, "%a", function (c)
+        return "[" .. string.lower(c) .. string.upper(c) .. "]"
+    end)
+    return s
+end
+
+print(nocase("Hi there!"))
