a2c9af68f053ab5aa3d86c5a8dcde8a204f5564c
rust-lang.xfoss.com <unisko@gmail.com>
2023-11-17 16:56:22 +0800

    Updated 'src/lua_tut/data_files.md'.

--- scripts/lua/ser.lua
+++ scripts/lua/ser.lua
@@ -3,11 +3,29 @@
 local fmt = {integer = "%d", float = "%a"}
 
 function seralize (o)
-    if type(o) == "number" then
-        io.write(string.format(fmt[math.type(o)], o))
+    local t = type(o)
+
+    if t == "number"
+        or t == "string"
+        or t == "boolean"
+        or t == "nil"
+        then
+            io.write(string.format("%q", o))
+    else other cases
+    end
+end
+
+
+function quote (s)
+    -- 找出等号序列的最大长度
+    local n = -1
+    for w in string.gmatch(s, "]=*%f[%]]") do
+        n = math.max(n, #w - 1)     -- 减去 1 是要排除那个 ']'
     end
 
-    if type(o) == "string" then
-        io.write("[[", o, "]]")
-    end
+    -- 产生出有着 'n' 加一个等号的字符串
+    local eq = string.rep("=", n + 1)
+
+    -- 构建出括起来的字符串
+    return string.format(" [%s]\n%s]%s]", eq, s, eq)
 end
