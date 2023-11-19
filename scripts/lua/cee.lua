f7195f95b3223a70adfd132c17d4a22ca3ac9433
Lenny Peng <unisko@gmail.com>
2023-11-19 09:53:15 +0800

    Updated 'src/lua_tut/cee.md'.

--- scripts/lua/cee.lua
+++ scripts/lua/cee.lua
@@ -4,3 +4,14 @@ function dofile (filename)
     local f = assert(loadfile(filename))
     return f()
 end
+
+f = load("i = i + 1")
+
+i = 0
+f(); print(i)   --> 1
+f(); print(i)   --> 2
+
+s = "i = i + 1"
+load(s)(); print(i)     --> 3
+
+f = function () i = i + 1 end
